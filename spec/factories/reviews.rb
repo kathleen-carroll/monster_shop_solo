0fbd96a2b1f4aafeaac757cdaebd2738d1f24cd9
Jordan Williams <iEv0lv3@users.noreply.github.com>
2020-02-19 08:41:28 -0700

    Update spec/factories/reviews.rb
    Nice
    
    Co-Authored-By: David Atkins <55907198+d-atkins@users.noreply.github.com>

--- spec/factories/reviews.rb
+++ spec/factories/reviews.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
-  factory :random_review, class: User do
+  factory :random_review, class: Review do
     title             { Faker::Hipster.sentence(word_count: 4) }
     content           { Faker::TvShows::SouthPark.quote }
     rating            { rand(1..5) }
