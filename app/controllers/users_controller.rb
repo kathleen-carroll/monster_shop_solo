c02992971a2b9f8e3a4702e9dc266b427d280736
David Atkins <atki1080@gmail.com>
2020-02-18 19:45:57 -0700

    Add user creation
    Co-Authored-By: Jordan Williams <iEv0lv3@users.noreply.github.com>
    Co-Authored-By: Kathleen <kathleen-carroll@users.noreply.github.com>
    Co-Authored-By: Ray <itemniner@users.noreply.github.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,17 @@
 class UsersController < ApplicationController
-  def new
 
+  def new
+    @new_user = User.new(user_params)
   end
 
   def create
-    new_user = User.new(user_params)
-    if new_user.save
+    @new_user = User.new(user_params)
+    if @new_user.save
       flash[:success] = "New account successfully created! You are now logged in."
-      session[:user_id] = new_user.id
+      session[:user_id] = @new_user.id
       redirect_to "/profile"
     else
-      error_message(new_user)
+      error_message(@new_user)
       render :new
     end
   end
