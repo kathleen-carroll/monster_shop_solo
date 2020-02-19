3e85560653f86cb5c63a63268237d038dd2e8d43
David Atkins <atki1080@gmail.com>
2020-02-18 23:02:12 -0700

    Add test for image links

--- spec/features/items/index_spec.rb
+++ spec/features/items/index_spec.rb
@@ -7,7 +7,6 @@ RSpec.describe "Items Index Page" do
       @brian = Merchant.create(name: "Brian's Dog Shop", address: '125 Doggo St.', city: 'Denver', state: 'CO', zip: 80210)
 
       @tire = @meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
-
       @pull_toy = @brian.items.create(name: "Pull Toy", description: "Great pull toy!", price: 10, image: "http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg", inventory: 32)
       @dog_bone = @brian.items.create(name: "Dog Bone", description: "They'll love it!", price: 21, image: "https://img.chewy.com/is/image/catalog/54226_MAIN._AC_SL1500_V1534449573_.jpg", active?:false, inventory: 21)
     end
@@ -23,8 +22,16 @@ RSpec.describe "Items Index Page" do
       expect(page).to have_link(@dog_bone.merchant.name)
     end
 
-    it "I can see a list of all of the items "do
+    it "all items images are links to item show page" do
+      visit '/items'
 
+      expect(page).to have_link("image-#{@tire.id}")
+      expect(page).to have_link("image-#{@pull_toy.id}")
+      click_link("image-#{@dog_bone.id}")
+      expect(current_path).to eq("/items/#{@dog_bone.id}")
+    end
+
+    it "I can see a list of all of the items "do
       visit '/items'
 
       within "#item-#{@tire.id}" do
