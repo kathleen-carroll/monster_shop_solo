83c269361954d9cbcfabd1fa6c096f0de5273ee5
Jordan <jordan.wallace.williams@gmail.com>
2020-02-18 23:16:24 -0700

    Add admin user factory

---
+++ spec/factories/user2.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+  factory :random_admin, class: User do
+    name              { GOTFaker::Character.random_name }
+    address           { Faker::Address.street_address }
+    city              { GOTFaker::Geography.region }
+    state             { Faker::Address.state_abbr }
+    zip               { Faker::Address.zip }
+    email             { Faker::Internet.email }
+    password          { "burger32" }
+    roll              { 2 }
+  end
+end
