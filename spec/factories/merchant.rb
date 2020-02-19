cc0c6411cf41298d70bffc91e77ee86652379e31
Jordan <jordan.wallace.williams@gmail.com>
2020-02-18 23:14:50 -0700

    Add merchant factory

---
+++ spec/factories/merchant.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :random_merchant, class: User do
+    name              { GOTFaker::House.name }
+    address           { Faker::Address.street_address }
+    city              { GOTFaker::Geography.region }
+    state             { Faker::Address.state_abbr }
+    zip               { Faker::Address.zip }
+  end
+end
