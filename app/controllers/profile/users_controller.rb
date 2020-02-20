d700abf8ae360df1f80a862094e942e0c8b4fc44
Jordan <jordan.wallace.williams@gmail.com>
2020-02-20 16:18:32 -0700

    Update users controller to include update action

--- app/controllers/profile/users_controller.rb
+++ app/controllers/profile/users_controller.rb
@@ -5,9 +5,36 @@ class Profile::UsersController < ApplicationController
     @user = current_user
   end
 
+  def edit
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+    if @user.update(user_params)
+      flash[:success] = 'Profile successfully updated'
+      redirect_to '/profile'
+    else
+      flash[:error] = @user.errors.full_messages.to_sentence
+      render :edit
+    end
+  end
+
   private
 
   def require_user
     render file: "/public/404" if current_user.nil?
+  end
+
+  def user_params
+    params.permit(
+      :name,
+      :address,
+      :city,
+      :state,
+      :zip,
+      :email,
+      :role
+    )
   end
 end
