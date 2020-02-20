adb54fc6718afe15155f9a51b5a7fe9e2ff89a17
Jordan <jordan.wallace.williams@gmail.com>
2020-02-19 22:10:18 -0700

    Remove commented text

--- spec/features/users/user_navigation_spec.rb
+++ spec/features/users/user_navigation_spec.rb
@@ -39,16 +39,3 @@ RSpec.describe 'As a regular user', type: :feature do
     end
   end
 end
-
-# User Story 3, User Navigation
-
-# As a default user
-# I see the same links as a visitor
-# Plus the following links
-# - a link to my profile page ("/profile")
-# - a link to log out ("/logout")
-
-# Minus the following links
-# - I do not see a link to log in or register
-
-# I also see text that says "Logged in as Mike Dao" (or whatever my name is)
