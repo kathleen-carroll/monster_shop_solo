c37a1480c70235e7f111054b4ffe6484912c622b
Raymond Nguyen <rayknguyen@gmail.com>
2020-02-20 11:44:15 -0700

    Add sotry 39

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -7,10 +7,14 @@ class Admin::MerchantsController < ApplicationController
     merchant = Merchant.find(params[:id])
     merchant.toggle!(:active?)
     if !merchant.active?
+      merchant.items.update_all(active?: false)
       flash[:notice] = "#{merchant.name} has been deactivated."
     else
+      merchant.items.update_all(active?: true)
       flash[:notice] = "#{merchant.name} has been activated."
     end
     redirect_to admin_merchants_path
   end
 end
+
+
--- spec/features/admin/merchant/merchant_disable_spec.rb
+++ spec/features/admin/merchant/merchant_disable_spec.rb
@@ -24,4 +24,58 @@ RSpec.describe "on the admin merchant index" do
     end
     expect(page).to have_content("#{merchant.name} has been activated.")
   end
+
+  it "disables items also" do
+    customer = create(:regular_user)
+    user = create(:admin_user)
+    merchant = create(:random_merchant)
+    merchant_2 = create(:random_merchant)
+    item1 = create(:random_item, merchant: merchant)
+    item2 = create(:random_item, merchant: merchant)
+    item3 = create(:random_item, merchant: merchant)
+    item4 = create(:random_item, merchant: merchant)
+    item5 = create(:random_item, merchant: merchant_2)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+    
+    visit "/items"
+
+    expect(page).to have_content(item1.name)
+    expect(page).to have_content(item2.name)
+    expect(page).to have_content(item3.name)
+    expect(page).to have_content(item4.name)
+    expect(page).to have_content(item5.name)
+
+    visit "/admin/merchants"
+    
+    within "#merchant-#{merchant.id}" do
+      click_button "Disable"
+    end
+    click_on "Log Out"
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(customer)
+    visit "/items"
+    expect(page).not_to have_content(item1.name)
+    expect(page).not_to have_content(item2.name)
+    expect(page).not_to have_content(item3.name)
+    expect(page).not_to have_content(item4.name)
+    expect(page).to have_content(item5.name)
+
+    click_on "Log Out"
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit "/admin/merchants"
+    within "#merchant-#{merchant.id}" do
+      click_button "Enable"
+    end
+    click_on "Log Out"
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(customer)
+    visit "/items"
+    expect(page).to have_content(item1.name)
+    expect(page).to have_content(item2.name)
+    expect(page).to have_content(item3.name)
+    expect(page).to have_content(item4.name)
+    expect(page).to have_content(item5.name)
+  end
 end
