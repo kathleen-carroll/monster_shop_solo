ae7f0e4609cd1989173680fb5c97237ea92220d7
David Atkins <atki1080@gmail.com>
2020-02-19 23:37:00 -0700

    Add most_popular and least_popular

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,6 +12,21 @@ class Item <ApplicationRecord
   validates_inclusion_of :active?, :in => [true, false]
   validates_numericality_of :price, greater_than: 0
 
+  def self.most_popular(limit)
+     Item.select("items.*, SUM(quantity) AS total")
+       .joins(:item_orders)
+       .group(:id)
+       .order("total desc")
+       .limit(limit)
+  end
+
+  def self.least_popular(limit)
+     Item.select("items.*, SUM(quantity) AS total")
+       .left_outer_joins(:item_orders)
+       .group(:id)
+       .order("total asc")
+       .limit(limit)
+  end
 
   def average_review
     reviews.average(:rating)
