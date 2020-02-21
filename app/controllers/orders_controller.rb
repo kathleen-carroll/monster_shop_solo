bef6ed8611044d1378191b03ae0846b0d1a4328e
David Atkins <atki1080@gmail.com>
2020-02-20 17:20:56 -0700

    Create order under user

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,7 +9,7 @@ class OrdersController <ApplicationController
   end
 
   def create
-    order = Order.create(order_params)
+    order = current_user.orders.create(order_params)
     if order.save
       cart.items.each do |item,quantity|
         order.item_orders.create({
