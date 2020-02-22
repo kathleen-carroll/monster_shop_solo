1b38ef5872bbd75809d1b4bde0d9ecb45408327b
Kathleen Carroll <khakicarroll@comcast.net>
2020-02-22 15:21:10 -0700

    Change from orders controller to profile orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,29 +1,29 @@
 class OrdersController <ApplicationController
 
-  def new
-  end
+  # def new
+  # end
+  #
+  # def show
+  #   @order = Order.find(params[:id])
+  # end
 
-  def show
-    @order = Order.find(params[:id])
-  end
-
-  def create
-    order = current_user.orders.create(order_params)
-    if order.save
-      cart.items.each do |item,quantity|
-        order.item_orders.create({
-          item: item,
-          quantity: quantity,
-          price: item.price
-          })
-      end
-      session.delete(:cart)
-      redirect_to "/orders/#{order.id}"
-    else
-      flash[:notice] = "Please complete address form to create an order."
-      render :new
-    end
-  end
+  # def create
+  #   order = current_user.orders.create(order_params)
+  #   if order.save
+  #     cart.items.each do |item,quantity|
+  #       order.item_orders.create({
+  #         item: item,
+  #         quantity: quantity,
+  #         price: item.price
+  #         })
+  #     end
+  #     session.delete(:cart)
+  #     redirect_to "/orders/#{order.id}"
+  #   else
+  #     flash[:notice] = "Please complete address form to create an order."
+  #     render :new
+  #   end
+  # end
 
 
   private
--- app/controllers/profile/orders_controller.rb
+++ app/controllers/profile/orders_controller.rb
@@ -2,4 +2,37 @@ class Profile::OrdersController < Profile::BaseController
   def index
     @orders = current_user.orders
   end
+
+  def new
+  end
+
+  def show
+    @order = Order.find(params[:id])
+  end
+
+  def create
+    order = current_user.orders.create(order_params)
+    if order.save
+      cart.items.each do |item,quantity|
+        order.item_orders.create({
+          item: item,
+          quantity: quantity,
+          price: item.price
+          })
+      end
+      session.delete(:cart)
+      flash[:notice] = "Your order was created."
+      redirect_to "/profile/orders"
+    else
+      flash[:notice] = "Please complete address form to create an order."
+      render :new
+    end
+  end
+
+
+  private
+
+  def order_params
+    params.permit(:name, :address, :city, :state, :zip)
+  end
 end
