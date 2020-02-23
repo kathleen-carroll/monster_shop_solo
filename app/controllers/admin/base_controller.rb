ad17c86007ec7e57cff64c85bfbf7381773cc04c
Jordan <jordan.wallace.williams@gmail.com>
2020-02-23 01:31:46 -0700

    Update admin base controller syntax to Ruby style guide

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 class Admin::BaseController < ApplicationController
   before_action :require_user
-  
+
   def require_user
-    render file: "/public/404" if current_user.nil? || current_admin? == false
+    render file: '/public/404' if current_admin? == false
   end
 end
