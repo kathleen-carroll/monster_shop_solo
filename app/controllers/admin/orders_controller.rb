c6cfdd758f8a0a719625928871740208d74f1ed6
Jordan <jordan.wallace.williams@gmail.com>
2020-02-23 11:47:48 -0700

    Update controller to remove unecessary logic that is already provided by the base controller

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,16 +1,9 @@
 class Admin::OrdersController < Admin::BaseController
-  before_action :require_admin
 
   def update
     @order = Order.find(params[:id])
     if @order.update({status: 2})
       flash[:success] = 'Order Shipped'
     end
-  end
-
-  private
-
-  def require_admin
-    render file: "/public/404" unless current_admin?
   end
 end
