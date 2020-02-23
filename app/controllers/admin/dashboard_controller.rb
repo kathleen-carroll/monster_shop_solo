98c25ba8bfcace66eb008054c7e9b99cf3108bd8
Jordan <jordan.wallace.williams@gmail.com>
2020-02-23 10:57:53 -0700

    Update dashboard_controller: remove old conditional user check logic

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,13 +1,7 @@
 class Admin::DashboardController < Admin::BaseController
-  before_action :require_admin
 
   def index
     @orders = Order.all
   end
 
-  private
-
-  def require_admin
-    render file: "/public/404" unless current_admin?
-  end
 end
