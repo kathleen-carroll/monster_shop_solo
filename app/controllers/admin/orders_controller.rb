2bcc66b908ef1bc7203e9ebf282593c5e8320733
Jordan <jordan.wallace.williams@gmail.com>
2020-02-23 12:04:48 -0700

    Add redirect to update action to refresh page

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -4,6 +4,7 @@ class Admin::OrdersController < Admin::BaseController
     @order = Order.find(params[:id])
     if @order.update({status: 2})
       flash[:success] = 'Order Shipped'
+      redirect_to '/admin'
     end
   end
 end
