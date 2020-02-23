8c2ff02eb7dca8cc1d469f82966e0dc07768b6e3
Jordan <jordan.wallace.williams@gmail.com>
2020-02-23 10:57:00 -0700

    Update base controller to use private method

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,7 +1,11 @@
+# frozen_string_literal: true
+
 class Admin::BaseController < ApplicationController
   before_action :require_user
-  
+
+  private
+
   def require_user
-    render file: "/public/404" if current_user.nil? || current_admin? == false
+    render file: '/public/404' unless current_admin?
   end
 end
