f0f98682b9ce03254374dfc07c68730c5fdf975b
Jordan <jordan.wallace.williams@gmail.com>
2020-02-23 13:13:06 -0700

    Update item_order model to include enum options

--- app/models/item_order.rb
+++ app/models/item_order.rb
@@ -1,5 +1,6 @@
 class ItemOrder <ApplicationRecord
   validates_presence_of :item_id, :order_id, :price, :quantity
+  enum status: %w(unfulfilled fulfilled)
 
   belongs_to :item
   belongs_to :order
