2755c6367344c515ba882a59222cb9c209b3db6c
Kathleen Carroll <khakicarroll@comcast.net>
2020-02-23 18:12:19 -0700

    Create test for story 35

--- spec/features/merchant/merchant_dashboard_spec.rb
+++ spec/features/merchant/merchant_dashboard_spec.rb
@@ -38,4 +38,34 @@ RSpec.describe "as a merchant employee user" do
     expect(page).to have_content("Employer: #{user.merchant.name}")
     expect(page).to have_content("Employer Address: #{user.merchant.address} #{user.merchant.city}, #{user.merchant.state} #{user.merchant.zip}")
   end
+
+  it 'can see orders that have been place for that merchants products' do
+    user = create(:merchant_user)
+    item = create(:random_item, merchant: user.merchant)
+    item_order = create(:random_item_order, item: item)
+    item2 = create(:random_item, merchant: user.merchant)
+    item_order2 = create(:random_item_order, item: item)
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit '/merchant'
+
+    expect(page).to have_link("#{item_order.order_id}")
+    expect(page).to have_content("Order##{item_order.order_id}")
+    expect(page).to have_content("Order placed: ##{item_order.created_at.formatted_to_s(:long)}")
+    expect(page).to have_content("Number of items: #{item_order.quantity}")
+    expect(page).to have_content("Total Price: $#{item_order.price}")
+
+    click_on "#{item_order.order_id}"
+    expect(current_path).to eq("/merchant/orders/#{item_order.id}")
+
+    expect(page).to have_link("#{item_order2.order_id}")
+    expect(page).to have_content("Order##{item_order2.order_id}")
+    expect(page).to have_content("Order placed: ##{item_order2.created_at.formatted_to_s(:long)}")
+    expect(page).to have_content("Number of items: #{item_order2.quantity}")
+    expect(page).to have_content("Total Price: $#{item_order2.price}")
+
+    click_on "#{item_order2.order_id}"
+    expect(current_path).to eq("/merchant/orders/#{item_order2.id}")
+  end
 end
