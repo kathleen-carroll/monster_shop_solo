69daceddfe05639e3cb79f96b44fe019ed777f5f
David Atkins <atki1080@gmail.com>
2020-02-24 00:07:12 -0700

    Add logic to render 404 for unrelated orders

--- app/controllers/merchant/orders_controller.rb
+++ app/controllers/merchant/orders_controller.rb
@@ -1,6 +1,12 @@
 class Merchant::OrdersController < Merchant::BaseController
 
   def show
-    @order = Order.find(params[:id])
+    @order = Order.where(id: params[:id]).take
+    render file: "/public/404" if no_order
   end
+
+  private
+    def no_order
+      @order.nil? || @order.item_orders.by_merchant(current_user.merchant.id).empty?
+    end
 end
