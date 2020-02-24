12b58750d1eb97844e77aef006c2eec699ac3824
Kathleen Carroll <khakicarroll@comcast.net>
2020-02-24 12:52:27 -0700

    Work on story 35. AR query functioning as expected and view displaying well.  Need to update table

--- app/controllers/merchant/dashboard_controller.rb
+++ app/controllers/merchant/dashboard_controller.rb
@@ -3,6 +3,13 @@ class Merchant::DashboardController < ApplicationController
 
   def index
     @merchant = current_user.merchant
+    @item_orders = @merchant.items
+                            .joins(:orders)
+                            .select('item_orders.order_id,
+                                     item_orders.item_id,
+                                     item_orders.created_at,
+                                     item_orders.quantity,
+                                     item_orders.price')
   end
 
   private
