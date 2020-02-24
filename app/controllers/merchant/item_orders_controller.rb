fe9ac2e42fe2367b93630e1f1e771b29d8452ffd
Jordan <jordan.wallace.williams@gmail.com>
2020-02-24 16:15:56 -0700

    Update logic to test for if an order is ready

--- app/controllers/merchant/item_orders_controller.rb
+++ app/controllers/merchant/item_orders_controller.rb
@@ -3,6 +3,7 @@ class Merchant::ItemOrdersController < Merchant::BaseController
     item_order = ItemOrder.find(params[:id])
     if item_order.unfulfilled? && item_order.fulfill
       flash[:success] = "Item has been fulfilled."
+      item_order.order.ready
     else
       flash[:error] = "Cannot fulfill order."
     end
