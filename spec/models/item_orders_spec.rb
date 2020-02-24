ab55406df22918b6c3ed46fbf23c9f82a7d9981b
David Atkins <atki1080@gmail.com>
2020-02-23 21:52:02 -0700

    Add restock test

--- spec/models/item_orders_spec.rb
+++ spec/models/item_orders_spec.rb
@@ -22,6 +22,16 @@ describe ItemOrder, type: :model do
 
       expect(item_order_1.subtotal).to eq(200)
     end
+
+    it 'restock' do
+      meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+      tire = meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
+      order_1 = Order.create!(name: 'Meg', address: '123 Stang Ave', city: 'Hershey', state: 'PA', zip: 17033, user: create(:regular_user))
+      item_order_1 = order_1.item_orders.create!(item: tire, price: tire.price, quantity: 2, status: "fulfilled")
+      item_order_1.restock
+      expect(item_order_1.status).to eq("unfulfilled")
+      expect(item_order_1.item.inventory).to eq(14)
+    end
   end
 
 end
