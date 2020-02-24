aa7992e932abd43a22f5fcd8854d7c7dc58e6a50
Kathleen Carroll <khakicarroll@comcast.net>
2020-02-24 16:17:11 -0700

    Fix migration

--- db/migrate/20200224200751_change_price_to_float_in_items.rb
+++ db/migrate/20200224200751_change_price_to_float_in_items.rb
@@ -1,5 +1,5 @@
 class ChangePriceToFloatInItems < ActiveRecord::Migration[5.1]
   def change
-    change_column :items, :price, from: integer, to: float
+    change_column :items, :price, :float
   end
 end
