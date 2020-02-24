2bb4b85fd5f056b6a25b071f3947cec887553d0c
Raymond Nguyen <rayknguyen@gmail.com>
2020-02-24 14:29:10 -0700

    Update item new

--- app/controllers/merchant/items_controller.rb
+++ app/controllers/merchant/items_controller.rb
@@ -1,17 +1,17 @@
 class Merchant::ItemsController < Merchant::BaseController
 
   def new
-    @merchant = Merchant.find(params[:merchant_id])
-    @item = @merchant.items.create(item_params)
+    merchant = Merchant.find(params[:merchant_id])
+    @item = merchant.items.create(item_params)
   end
 
   def create
-    @merchant = Merchant.find(params[:merchant_id])
+    merchant = Merchant.find(params[:merchant_id])
     params.delete :image if params[:image].blank?
-    @item = @merchant.items.create(item_params)
+    @item = merchant.items.create(item_params)
     if @item.save
       flash[:success] = "#{@item.name} has been saved."
-      redirect_to "/merchants/#{@merchant.id}/items"
+      redirect_to "/merchants/#{merchant.id}/items"
     else
       flash[:error] = @item.errors.full_messages.to_sentence
       render :new
