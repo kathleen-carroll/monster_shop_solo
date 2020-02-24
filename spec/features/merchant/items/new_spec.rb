a66406ef7671c7de9fe449accbebe26f504dbe31
Raymond Nguyen <rayknguyen@gmail.com>
2020-02-24 12:40:43 -0700

    Add new spec

--- spec/features/merchant/items/new_spec.rb
+++ spec/features/merchant/items/new_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe "As a merchant", type: :feature do
     it "can click on that link to add a new item" do
         visit "/merchants/#{@merchant.id}/items"
         click_on "Add New Item"
-        expect(current_path).to eq("/merchants/#{@merchant.id}/items/new")
+        expect(current_path).to eq("/merchant/#{@merchant.id}/items/new")
 
         name = "Chamois Buttr"
         price = 18
@@ -36,6 +36,32 @@ RSpec.describe "As a merchant", type: :feature do
         expect(Item.last.active?).to be(true)
         expect(page).to have_content(name)
         expect(page).to have_css("img[src*='#{new_item.image}']")
+    end
+    it 'can add a new item without an image' do
+       visit "/merchants/#{@merchant.id}/items"
+        click_on "Add New Item"
+        expect(current_path).to eq("/merchant/#{@merchant.id}/items/new")
+
+        name = "Chamois Buttr"
+        price = 18
+        description = "No more chaffin'!"
+        inventory = 25
+
+        fill_in "Name", with: name
+        fill_in "Price", with: price
+        fill_in "Description", with: description
+        fill_in "Image", with: ""
+        fill_in "Inventory", with: inventory
+
+        click_button "Create Item"
+        
+        new_item = Item.last
+        
+        expect(page).to have_content("#{new_item.name} has been saved.")
+        expect(current_path).to eq("/merchants/#{@merchant.id}/items")
+        expect(new_item.name).to eq(name)
+        expect(Item.last.active?).to be(true)
+        expect(page).to have_content(name)
     end
   end
 end
