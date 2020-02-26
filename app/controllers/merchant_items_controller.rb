16fcaec13f474dfc5014bfaff67d7c7ab411f2de
David Atkins <atki1080@gmail.com>
2020-02-26 16:59:29 -0700

    Change index to pass only one instance variable

--- app/controllers/merchant_items_controller.rb
+++ app/controllers/merchant_items_controller.rb
@@ -2,7 +2,6 @@ class MerchantItemsController <ApplicationController
 
   def index
     @merchant = Merchant.find(params[:merchant_id])
-    @items = @merchant.items.where(active?: true)
   end
 
 end
