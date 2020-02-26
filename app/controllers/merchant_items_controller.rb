840818d16e10e08495642888a272ee507d8c04a6
David Atkins <atki1080@gmail.com>
2020-02-26 16:54:22 -0700

    Remove filtering logic from merchants items index

--- app/controllers/merchant_items_controller.rb
+++ app/controllers/merchant_items_controller.rb
@@ -2,11 +2,7 @@ class MerchantItemsController <ApplicationController
 
   def index
     @merchant = Merchant.find(params[:merchant_id])
-    if current_merchant_employee? || current_admin?
-      @items = @merchant.items
-    else
-      @items = @merchant.items.where(active?: true)
-    end
+    @items = @merchant.items.where(active?: true)
   end
 
 end
