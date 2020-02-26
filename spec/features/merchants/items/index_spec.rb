951110b9dc5842156e983df1bc4e25b6bbc9ad9e
David Atkins <atki1080@gmail.com>
2020-02-26 16:53:34 -0700

    Remove incorrect tests

--- spec/features/merchants/items/index_spec.rb
+++ spec/features/merchants/items/index_spec.rb
@@ -30,35 +30,5 @@ RSpec.describe "Merchant Items Index Page" do
 
       expect(page).to_not have_css("#item-#{@shifter.id}")
     end
-
-    it "shows deactivated items if I'm that merchant's employee" do
-      user = create(:merchant_user, merchant: @meg)
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-      visit "/merchants/#{@meg.id}/items"
-
-      expect(page).to have_css("#item-#{@tire.id}")
-      expect(page).to have_css("#item-#{@chain.id}")
-      expect(page).to have_css("#item-#{@shifter.id}")
-    end
-
-    it "shows deactivated items if I'm an administrator" do
-      user = create(:admin_user)
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-      visit "/merchants/#{@meg.id}/items"
-
-      expect(page).to have_css("#item-#{@tire.id}")
-      expect(page).to have_css("#item-#{@chain.id}")
-      expect(page).to have_css("#item-#{@shifter.id}")
-    end
-
-    it "doesn't show another merchant's deactivated items" do
-      user = create(:merchant_user)
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-      visit "/merchants/#{@meg.id}/items"
-
-      expect(page).to have_css("#item-#{@tire.id}")
-      expect(page).to have_css("#item-#{@chain.id}")
-      expect(page).to_not have_css("#item-#{@shifter.id}")
-    end
   end
 end
