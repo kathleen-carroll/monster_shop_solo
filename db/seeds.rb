e479ac34b78a58a3ba178e3a9aaa7e0672bfc84b
Jordan <jordan.wallace.williams@gmail.com>
2020-02-26 11:32:25 -0700

    Update seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-include FactoryBot::Syntax::Methods
 ItemOrder.destroy_all
 Order.destroy_all
 Item.destroy_all
