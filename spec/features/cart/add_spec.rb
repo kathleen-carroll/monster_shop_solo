2192bbf2e0b5bb26c1c82b79531c84d62583722a
David Atkins <atki1080@gmail.com>
2020-02-25 22:26:42 -0700

    Modify test

--- spec/features/cart/add_spec.rb
+++ spec/features/cart/add_spec.rb
@@ -35,6 +35,9 @@ RSpec.describe 'Cart creation' do
     end
 
     it "I can't add a deactivated item" do
+      user = create(:merchant_user, merchant: @mike)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+      
       expect(@pen.active?).to eq(false)
       visit "/items/#{@pen.id}"
       click_on "Add To Cart"
