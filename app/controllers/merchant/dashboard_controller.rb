a4d3e020bcd9cc404557c29fbb3199c96d5f03a8
David Atkins <atki1080@gmail.com>
2020-02-27 12:44:31 -0700

    Remove extra instance variables from controllers

--- app/controllers/merchant/dashboard_controller.rb
+++ app/controllers/merchant/dashboard_controller.rb
@@ -2,20 +2,5 @@ class Merchant::DashboardController < Merchant::BaseController
 
   def index
     @merchant = current_user.merchant
-    @item_orders = @merchant.items
-                            .joins(:orders)
-                            .where("orders.status = 1")
-                            .distinct
-                            .group('orders.id')
-                            .sum('item_orders.quantity * item_orders.price')
-
-     @item_orders2 = @merchant.items
-                             .joins(:orders)
-                             .where("orders.status = 1")
-                             .distinct
-                             .group('orders.id')
-                             .sum('item_orders.quantity')
-
-    @orders = Order.joins(:items).distinct.where("orders.status = 1 and items.merchant_id = #{@merchant.id}")
   end
 end
