31bcab3ee70d631228c681fd0179c64a64d952cd
Jordan <jordan.wallace.williams@gmail.com>
2020-02-26 21:31:22 -0700

    Update show action to use logic from merchant orders

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -6,6 +6,21 @@ class Admin::MerchantsController < Admin::BaseController
 
   def show
     @merchant = Merchant.find(params[:id])
+    @item_orders = @merchant.items
+                            .joins(:orders)
+                            .where("orders.status = 1")
+                            .distinct
+                            .group('orders.id')
+                            .sum('item_orders.quantity * item_orders.price')
+
+     @item_orders2 = @merchant.items
+                             .joins(:orders)
+                             .where("orders.status = 1")
+                             .distinct
+                             .group('orders.id')
+                             .sum('item_orders.quantity')
+
+    @orders = Order.joins(:items).distinct.where("orders.status = 1 and items.merchant_id = #{@merchant.id}")
   end
 
   def update
