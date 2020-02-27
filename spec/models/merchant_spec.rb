960eb0af4594552fefb5c0998fc708ffed33fa0e
David Atkins <atki1080@gmail.com>
2020-02-27 12:34:46 -0700

    Add relationship validation tests

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -12,6 +12,8 @@ describe Merchant, type: :model do
   describe "relationships" do
     it {should have_many :items}
     it {should have_many :users}
+    it {should have_many(:item_orders).through(:items)}
+    it {should have_many(:orders).through(:item_orders)}
   end
 
   describe 'instance methods' do
