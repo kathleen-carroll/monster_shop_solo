44fa558ef692972399bcd6333a290e2f4a452712
Raymond Nguyen <rayknguyen@gmail.com>
2020-02-27 10:29:51 -0700

    Update test edit item

--- spec/features/items/edit_spec.rb
+++ spec/features/items/edit_spec.rb
@@ -77,28 +77,6 @@ RSpec.describe "As a Merchant" do
         expect(page).to have_content("Name can't be blank and Image can't be blank")
         expect(page).to have_button("Update Item")
       end
-
-      it "can only enter a number in inventory" do
-        meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
-        tire = meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
-        merchant_employee = create(:merchant_user, merchant: meg)
-        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(merchant_employee)
-
-
-        visit "/items/#{tire.id}"
-
-        click_on "Edit Item"
-
-        fill_in 'Name', with: "ray"
-        fill_in 'Price', with: 110
-        fill_in 'Description', with: "They're a bit more expensive, and they kinda do pop sometimes, but whatevs.. this is retail."
-        fill_in 'Image', with: "image"
-        fill_in 'Inventory', with: "string"
-
-        click_on "Update Item"
-
-        expect(page).to have_content("Inventory is not a number")
-      end
     end
   end
 end
