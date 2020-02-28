7729d62612149422568787ca8bbf5c459681b763
Jordan <jordan.wallace.williams@gmail.com>
2020-02-27 20:03:51 -0700

    User story 61

--- app/controllers/admin/merchant_items_controller.rb
+++ app/controllers/admin/merchant_items_controller.rb
@@ -1,23 +1,24 @@
-class Merchant::ItemsController < Merchant::BaseController
-  before_action :require_employee, only: [:new, :create]
-  before_action :require_item_match, only: [:show, :edit, :update]
-
+class Admin::MerchantItemsController < Admin::BaseController
   def index
-    @merchant = Merchant.find(current_user.merchant.id)
+    @merchant = Merchant.find(params[:id])
+  end
+
+  def show
+    @item = Item.find(params[:id])
   end
 
   def new
-    merchant = Merchant.find(params[:merchant_id])
+    merchant = Merchant.find(params[:id])
     @item = merchant.items.create(item_params)
   end
 
   def create
-    merchant = Merchant.find(params[:merchant_id])
+    merchant = Merchant.find(params[:id])
     params.delete :image if params[:image].blank?
     @item = merchant.items.create(item_params)
     if @item.save
       flash[:success] = "#{@item.name} has been saved."
-      redirect_to "/merchant/items"
+      redirect_to "/admin/merchants/#{merchant.id}/items"
     else
       flash.now[:error] = @item.errors.full_messages.to_sentence
       render :new
@@ -25,23 +26,24 @@ class Merchant::ItemsController < Merchant::BaseController
   end
 
   def edit
+    @item = Item.find(params[:id])
   end
 
   def update
+    @item = Item.find(params[:id])
+    merchant = @item.merchant
     if @item.update(item_params)
       flash[:success] = "#{@item.name} has been updated."
-      redirect_to "/merchant/items"
+      redirect_to "/admin/merchants/#{merchant.id}/items"
     else
       flash.now[:error] = @item.errors.full_messages.to_sentence
       render :edit
     end
   end
 
-  def show
-  end
-
   def destroy
-    item = Item.find_by(id: params[:id])
+    item = Item.find(params[:id])
+    merchant = item.merchant
     if !item
       flash[:error] = "Item already deleted."
     elsif item.no_orders?
@@ -50,23 +52,12 @@ class Merchant::ItemsController < Merchant::BaseController
     else
       flash[:error] = "Cannot delete an item with orders."
     end
-    redirect_to "/merchant/items"
+    redirect_to "/admin/merchants/#{merchant.id}/items"
   end
 
     private
 
     def item_params
       params.permit(:name,:description,:price,:inventory,:image)
-    end
-
-    def require_employee
-      authorized = params[:merchant_id] == current_user.merchant.id.to_s
-      render file: "/public/404" unless authorized
-    end
-
-    def require_item_match
-      @item = Item.find_by(id: params[:id])
-      authorized = @item && @item.merchant == current_user.merchant
-      render file: "/public/404" unless authorized
     end
 end
