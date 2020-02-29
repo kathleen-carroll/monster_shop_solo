f19f977208f8d40e007daa8102bd31c13df4b9a7
Jordan <jordan.wallace.williams@gmail.com>
2020-02-29 15:22:33 -0700

    Update schema to include discounts table

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200229212522) do
+ActiveRecord::Schema.define(version: 20200229221654) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "discounts", force: :cascade do |t|
     t.string "name"
-    t.integer "threshold"
-    t.integer "percent"
+    t.integer "item_count"
+    t.float "percent"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "merchant_id"
