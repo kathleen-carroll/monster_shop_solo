a03107703c1111726ee5cd36e28a24c2899d0d3f
Kathleen Carroll <khakicarroll@comcast.net>
2020-03-03 12:08:18 -0700

    Finish edit and update discount

--- app/controllers/merchant/discounts_controller.rb
+++ app/controllers/merchant/discounts_controller.rb
@@ -6,4 +6,25 @@ class Merchant::DiscountsController < Merchant::BaseController
   def show
     @discount = Discount.find(params[:id])
   end
+
+  def edit
+    @discount = Discount.find(params[:id])
+  end
+
+  def update
+    @discount = Discount.find(params[:id])
+    if @discount.update(discount_params)
+      flash[:success] = "#{@discount.name} has been updated."
+      redirect_to "/merchant/discounts/#{@discount.id}"
+    else
+      flash.now[:error] = @discount.errors.full_messages.to_sentence
+      render :edit
+    end
+  end
+
+  private
+
+  def discount_params
+    params.permit(:name, :percent, :item_count)
+  end
 end
