8f1fa4cf9b3a83f141228c2cb5a084fdf43ead34
Kathleen Carroll <khakicarroll@comcast.net>
2020-03-03 11:34:46 -0700

    Update discount faker

--- spec/factories/discount.rb
+++ spec/factories/discount.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :discount, class: Discount do
-    name              { Faker::Hipster.sentence(word_count: 3) }
+    name              { Faker::Subscription.plan + " " + Faker::Hipster.sentence(word_count: 1) }
     item_count        { ((Faker::Number.within(range: 20..100))/10).round * 10 }
     percent           { ((Faker::Number.within(range: 5..35))/5).round * 5}
     association       :merchant, factory: :random_merchant
