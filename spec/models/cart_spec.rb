f477ed2395aab0ec8debd3afe25f5333eb00e670
Kathleen Carroll <khakicarroll@comcast.net>
2020-03-03 22:39:41 -0700

    Finish cart model tests

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -1,3 +1,5 @@
+require 'rails_helper'
+
 describe "instance methods" do
   before(:each) do
     @item1 = create(:random_item, inventory: 4)
@@ -22,5 +24,40 @@ describe "instance methods" do
     expect(@cart.inventory_limit?("min", @item1)).to eq(false)
     @cart.contents[@item1.id.to_s] = 0
     expect(@cart.inventory_limit?("min", @item1)).to eq(true)
+  end
+
+  it 'merchants' do
+    item2 = create(:random_item)
+    @cart.add_item(item2.id)
+
+    expect(@cart.merchants).to eq([@item1.merchant, item2.merchant])
+  end
+
+  it 'discounts' do
+    item2 = create(:random_item)
+    @cart.add_item(item2.id)
+
+    discount = create(:discount, merchant: @item1.merchant)
+    discount2 = create(:discount, merchant: item2.merchant)
+
+    expect(@cart.discounts).to eq([discount, discount2])
+  end
+
+  it 'min_discount_qty' do
+    item2 = create(:random_item)
+    @cart.add_item(item2.id)
+
+    discount = create(:discount, merchant: @item1.merchant, item_count: 10)
+    discount2 = create(:discount, merchant: @item1.merchant, item_count: 20)
+
+    expect(@cart.min_discount_qty(@item1.merchant)).to eq(10)
+  end
+
+  it 'discount_subtotal' do
+    2.times do @cart.add_item(@item1.id) end
+
+    discount = create(:discount, merchant: @item1.merchant, item_count: 2, percent: 50)
+
+    expect(@cart.discount_subtotal(@item1)).to eq([discount])
   end
 end
